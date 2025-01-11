4882baf02d6c84115e4394e1a94cc7019f17ab33
Andi Muhammad Alief Fauzan <145396717+aliefauzan@users.noreply.github.com>
2025-01-11 08:25:40 -0800

    Delete money-tracker-api directory

--- money-tracker-api/create_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE records (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(25) NOT NULL,
-    amount DOUBLE NOT NULL,
-    date DATETIME NOT NULL,
-    notes TEXT,
-    is_masuk TINYINT(1) NOT NULL DEFAULT 0, -- 1 untuk Masuk, 0 untuk tidak
-    is_keluar TINYINT(1) NOT NULL DEFAULT 0 -- 1 untuk Keluar, 0 untuk tidak
-);
